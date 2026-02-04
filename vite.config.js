52f9fdf1d84b2f11c90537fc292e319502a27088
Alexander Howard <alexanderhoward@campus-094-180.ucdavis.edu>
2026-02-04 15:15:17 -0800

    Update basename

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/alexanderjhoward.github.io/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
